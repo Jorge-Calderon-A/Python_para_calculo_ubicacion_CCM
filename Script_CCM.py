ad291cb29845eb10959b860e0a92d40958305917
Jorge Calderón <jorgecalderonc22@gmail.com>
2026-01-29 15:16:34 -0300

    Update Script_CCM.py

--- Script_CCM.py
+++ Script_CCM.py
@@ -1,1 +1,10 @@
-
+from datetime import datetime
+saludo = datetime.now().hour
+if 5 <= saludo < 12:
+    print ("Hola, buenos días")
+elif 12 <= saludo < 18:
+    print ("Hola, buenas tardes")
+else:
+    print ("Hola, buenas noches")
+print("Bienvenido al sistema de gestión para cálculo de instalaciones eléctricas.")
+print("Por favor, seleccione una opción del menú:")
